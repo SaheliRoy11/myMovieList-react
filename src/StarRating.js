a25cb94a26e7fcc193f4b9b7ffeef01732401986
Saheli <engr.saheliroy@gmail.com>
2025-06-23 21:23:01 +0530

    Improving reusability of the component with props

--- src/StarRating.js
+++ src/StarRating.js
@@ -11,51 +11,74 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
+export default function StarRating({
+  maxRating = 5,
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+  color = "#fcc419", //define the colour of stars and text
+
+  size = 48, //define the size of stars and text
+
+  className = "", //define specific styles through a class
+
+  messages = [], // an array of messages containing messages which can be displayed instead of rating numbers.Use this prop only if there are elements in the array, also the number of elements should be correct.Example, if we allow a rating between 1 and 5, and there are 3 messages it does not make sense
+
+  defaultRating = 0, // if we want the rating to start off with some default value, if this prop is no provided it starts off normally with 0 rating
+
+  onSetRating//through this function we can give access to rating state.
+}) {
+
+  const [rating, setRating] = useState(defaultRating);
 
   //when the stars are being hovered, it stores the value of the nth star because we need to display a temporary rating and also change the number of full and empty stars,when hovering over any star.
-  // Initially, when the page loads there is no tempRating, now suppose we hover over the 3rd star, immediately it should show a rating of 3, next if move to the 5th star, it should show a rating of 5.This value is stored by tempRating state. 
+  // Initially, when the page loads there is no tempRating, now suppose we hover over the 3rd star, immediately it should show a rating of 3, next if move to the 5th star, it should show a rating of 5.This value is stored by tempRating state.
   // Or, if we have clicked on the 6th star and give a 6 rating to the movie, but now we decide to rate it 8, we can hover over the 8th star and hence tempRating will show 8, next if we decide to click on 8th star the rating is updated to 8, otherwise if we don't click on any star and don't hover the rating is back to 6.
   const [tempRating, setTempRating] = useState(0);
 
-
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color: color,
+    size: `${size / 1.5} px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            //if the stars are hovered, tempRating has some value and hence there should be full stars till the nth star that is being hovered on and the rest should be empty stars.Otherwise if there is no tempRating truthy value it means the stars are not being hovered,that is either there is no rating or some rating is already given. 
-            full={tempRating ? tempRating >= i + 1: rating >= i + 1}
+            //if the stars are hovered, tempRating has some value and hence there should be full stars till the nth star that is being hovered on and the rest should be empty stars.Otherwise if there is no tempRating truthy value it means the stars are not being hovered,that is either there is no rating or some rating is already given.
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  height: "48px",
-  width: "48px",
-  display: "block",
-  cursor: "pointer",
-};
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    height: `${size}px`,
+    width: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
     <span
       role="button"
@@ -68,8 +91,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -78,7 +101,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
-    <StarRating />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={24} color="red" className="test" />
+    <StarRating defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
-
-
